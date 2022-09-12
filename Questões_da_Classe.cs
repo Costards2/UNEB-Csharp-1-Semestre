832f9124a61b72279b83e234bea048ab8fd6826c
Pedro Costard oliveira <100092162+Costards2@users.noreply.github.com>
2022-09-12 15:04:15 -0300

    Update Questões_da_Classe.cs

--- Questões_da_Classe.cs
+++ Questões_da_Classe.cs
@@ -136,4 +136,57 @@ namespace myApp
     }
   }
 }
+
+/* Questão 2 */
+        
+        namespace myApp
+{ 
+  class questao2
+  {
+    static void Main()
+    {
+        double inscricao, classe, shorasnormais, shorasextras, horas, shorasnormaisresult;
+        string nome; 
+        
+        Console.WriteLine ("Inscricao : ");
+        inscricaousing System;
+
+namespace myApp
+{ 
+  class questao2
+  {
+    static void Main()
+    {
+        double inscricao, classe, shorasnormais, shorasextras, shorasnormaisresult;
+        int horas, horasextras;
+        string nome; 
+        
+        Console.WriteLine ("Inscricao : ");
+        inscricao = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Classe : ");
+        classe = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Horas Normais : ");
+        horas = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Horas Extras : ");
+        horasextras = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Nome : ");
+        nome = (Console.ReadLine());
+        
+        shorasnormais = 5 * horas;
+        shorasnormaisresult = (5 * horas) * 1.3 + shorasnormais;
+        shorasextras = (shorasnormais * 0.3) * horasextras;
+        double salariobruto = (shorasnormais * 0.3) + shorasnormais;
+        double INSS = salariobruto * 0.11;
+        double salarioliquido = salariobruto - INSS; 
+        
+        Console.WriteLine("***********FICHA***********\r");
+        Console.WriteLine("INCRICAO: \r" +inscricao);
+        Console.WriteLine("\rNOME: \r" +nome);
+        Console.WriteLine("\rSALARIO HORAS NORMAIS: \r" +shorasnormais); 
+        Console.WriteLine("\rSALARIO HORAS EXTRAS: \r" +shorasextras);
+        Console.WriteLine("\rDEDUCAO DO INSS: \r" +INSS);
+        Console.WriteLine("\rSALARIO LIQUIDO: \r" +salarioliquido);     
+    }
+  }
+}
         
