7a40b62cdccd102af6e374b5195095bfb357b16c
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2022-10-10 14:11:26 -0300

    Update Lista_4_respostas.cs

--- Lista_4_respostas.cs
+++ Lista_4_respostas.cs
@@ -98,4 +98,203 @@ class lista4_questao4 {
     }
   }
 }
-        
+  
+/* Questão 5 */
+
+  using System;
+class questao5 {
+  static void Main() {
+  int impares = 0, pares = 0;
+  float numero = 0;
+  Console.WriteLine(&quot;diga os numeros presentes no seu conjunto\npara parar de adicionar ao
+  conjunto digite -1&quot;);
+  while (numero != -1){
+  Console.WriteLine(&quot;escreva um numero inteiro&quot;);
+
+  numero = float.Parse(Console.ReadLine());
+  if(numero == -1){
+  break;
+  }
+  else if(numero % 2 == 0){
+  pares++;
+    }else{
+      impares++;
+  }
+}
+Console.WriteLine($&quot;a quantidade de numeros pares é {pares}&quot;);
+Console.WriteLine($&quot;a quantidade de numeros impares é {impares}&quot;);
+  }
+}
+
+/* Questão 6 */
+
+using System;
+class questao5 {
+  static void Main() {
+
+    int numero, i;
+    Console.WriteLine(&quot;digite o numero para saber o seu fatorial&quot;);
+    numero = int.Parse(Console.ReadLine());
+    int produto = numero;
+    for (i = numero - 1; i &gt;= 1; i--){
+    produto = produto * i;
+    }
+    Console.WriteLine($&quot;o fatorial de {numero} é {produto}&quot;);
+    }
+  }
+
+/* Questão 7 */ 
+
+using System;
+class questao5 {
+  static void Main() {
+
+  int numeros, i;
+  int sequencia = 1, soma = 1, sequencia2 = 0;
+
+  Console.WriteLine(&quot;escreva a quantidade de numeros da sequencia de Fibonacci que vc deseja saber&quot;);
+  numeros = int.Parse(Console.ReadLine());
+  for (i = 1; i &lt;= numeros; i++){
+    Console.WriteLine(soma);
+    soma = sequencia + sequencia2;
+    sequencia2 = sequencia;
+    sequencia = soma;
+    }
+  }
+}
+
+/* Questão 8 */
+
+using System;
+class shopping {
+  static void Main() {
+
+    int idade, i = 0, objetivo, quantidadesf = 0, quantidadesm = 0;
+    int compras = 0, servicos = 0, lazer = 0, alimentacao = 0, totalobjetivo;
+    float porcentagem;
+    string nome, sexo;
+
+    Console.WriteLine(&quot;Bem vindo ao shopping!\nPor favor responda nossa pesquisa!&quot;);
+    Console.WriteLine(&quot;obs:para acabar com a pesquisa escreva fim ao inves de seu nome&quot;);
+    while (i != 1){
+    Console.WriteLine(&quot;escreva seu nome&quot;);
+    nome = Console.ReadLine();
+    if(nome == &quot;fim&quot;){
+    break;
+    }
+
+    Console.WriteLine(&quot;escreva sua idade&quot;);
+    idade = int.Parse(Console.ReadLine());
+    Console.WriteLine(&quot;escreva seu sexo(masculino ou feminino)&quot;);
+    sexo = Console.ReadLine();
+    if(sexo == &quot;masculino&quot;){
+      quantidadesm++;
+        }else{
+           quantidadesf++;
+        }
+
+    Console.WriteLine(&quot;escreva o objetivo da visita&quot;);
+    Console.WriteLine(&quot;010 Compras\n020 Serviços (Banco, correio,etc.)\n030 Lazer (Cinema, jogos,etc.)\n040 Alimentação (restaurantes e lanchonetes)&quot;);
+    objetivo = int.Parse(Console.ReadLine());
+
+    if(objetivo == 010){
+    compras++;
+      }else if(objetivo == 020){
+        servicos++;
+          }else if(objetivo == 030){
+            lazer++;
+              }else if(objetivo == 040){
+                alimentacao++;
+  }
+}
+    totalobjetivo = compras + servicos + lazer + alimentacao;
+    porcentagem = (servicos * 100)/totalobjetivo;
+
+    Console.WriteLine($&quot;a porcentagem de pessoas que escolheu serviços é {porcentagem}%&quot;);
+    if(quantidadesf &gt; quantidadesm){
+       Console.WriteLine(&quot;o numero de mulheres frequentando o shopping foi maior que o de homens&quot;);
+      }else{
+        Console.WriteLine(&quot;o numero de homens frequentando o shopping foi maior que o de mulheres&quot;);
+  }
+ }
+}
+
+/* Questão 9 */
+
+using System;
+class livraria {
+  static void Main() {
+
+    int tipo, quantidades1 = 0, quantidades2 = 0, quantidades3 = 0, i = 0;
+    double mficcao = 0, mromance = 0, maventura = 0, mtotal;
+    double porcentagem;
+    string sexo;
+
+    Console.WriteLine(&quot;Bem vindo a livraria!\nPor favor responda nossa pesquisa!&quot;);
+    Console.WriteLine(&quot;obs:para acabar com a pesquisa escreva 0 no tipo de livro&quot;);
+
+    while (i != 1){
+    Console.WriteLine(&quot;tipo de livro\n1 - Ficção \n2 – Romance \n3 –Aventura&quot;);
+    tipo = int.Parse(Console.ReadLine());
+
+    if(tipo == 0){
+    break;
+      } else if(tipo == 1){
+        quantidades1++;
+          }else if(tipo == 2){
+            quantidades2++;
+              }else if(tipo == 3){
+                 quantidades3++;
+}
+
+    Console.WriteLine(&quot;escreva seu sexo\nm-masculino\nf-feminino)&quot;);
+    sexo = Console.ReadLine();
+
+    if((sexo == &quot;m&quot; || sexo == &quot;M&quot;) &amp;&amp; tipo == 1){
+    mficcao++;
+      }else if((sexo == &quot;m&quot; || sexo == &quot;M&quot;) &amp;&amp; tipo == 2){
+        mromance++;
+          }else if((sexo == &quot;m&quot; || sexo == &quot;M&quot;) &amp;&amp; tipo == 3){
+            maventura++;
+  }
+}
+    mtotal = maventura + mromance + mficcao;
+    porcentagem = (mromance * 100)/mtotal;
+
+    Console.WriteLine($&quot;a porcentagem de pessoas do sexo masculino que compra o tip0 romance é {porcentagem}%&quot;);
+
+    if(quantidades1 &gt; quantidades2 &amp;&amp; quantidades1&gt;quantidades3){
+    Console.WriteLine(&quot;o numero de livros de ficção vendidos foi maior que os outros tipos&quot;);
+      }else if(quantidades2 &gt; quantidades1 &amp;&amp; quantidades2 &gt; quantidades3){
+        Console.WriteLine(&quot;o numero de livros de romance vendidos foi maior que os outros tipos&quot;);
+          }else if(quantidades3 &gt; quantidades1 &amp;&amp; quantidades3 &gt; quantidades2){
+            Console.WriteLine(&quot;o numero de livros de aventura vendidos foi maior que os outros tipos&quot;);
+              }else{
+                Console.WriteLine(&quot;todos os tipos venderam igualmente&quot;);
+    }
+  }
+}
+
+/* Questão 10 */
+
+using System;
+class relogio {
+  static void Main() {
+
+    int horas = 0, minutos = 0, segundos = 0, i = 0;
+    
+    do {
+    segundos++;
+      if (segundos == 60){
+        minutos++;
+        segundos = 0;
+}
+    if (minutos == 60){
+    horas++;
+    minutos = 0;
+    segundos = 0;
+}
+    Console.WriteLine($&quot;{horas}:{minutos}:{segundos}&quot;);
+    }while(horas != 24);
+  }
+}
