a89a1c0181318200ea036fd2ba3efaf3395cbb8d
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2022-10-26 10:04:44 -0300

    Update Lista_5_respostas.cs

--- Lista_5_respostas.cs
+++ Lista_5_respostas.cs
@@ -310,5 +310,79 @@ class questão8 {
     }
 }
 
+/* Questão 9 */
+
+using System;
+class HelloWorld {
+   static string entrada(string nome){
+        nome = Console.ReadLine();
+        return nome;
+  }
+  static float entrada2(float peso){
+        peso = float.Parse(Console.ReadLine());
+        return peso;
+  }
+  static void Main() {
+    string[] nome = new string[10];//{"a", "b", "c","d","f","g","h","i","j","k"};
+    string[] sexo = new string[10];//{"f", "m", "m","m","m","f","f","f","m","f"};
+    float[] altura  = new float[10];//{ 1.80d , 2.00d, 1.60d, 1.70d, 1.50d, 1.80d, 1.80d, 1.60d, 1.55d, 1.65d};
+    float[] peso  = new float[10];//{ 40.5d, 60.6d, 40.3d, 40.4d, 80.4d, 80.5d, 90.5d, 50.9d, 80.8d, 40.6d};
+    float altura2 = 0, peso2 = 0;
+    string nome2 = "", sexo2 = "";
+    int i = 0;
+    for(i = 0; i<10; i++){
+        Console.WriteLine("escreva o nome, sexo, altura e peso da pessoa");
+        nome2 = entrada(nome2);
+        nome[i] = nome2;
+        sexo2 = entrada(sexo2);
+        sexo[i] = sexo2;
+        altura2 = entrada2(altura2);
+        altura[i] = altura2;
+        peso2 = entrada2(peso2);
+        peso[i] = peso2;
+    }
+    float somap = 0;
+    float somaa=0;
+    float mediap = 0;
+    float mediaa= 0;
+    float sm = 0, sf = 0;
+    for(i = 0; i<4; i++){
+        if(sexo[i] == "f"){
+            somap = somap + peso[i];
+        }
+        if(sexo[i] == "m"){
+            somaa = somaa + altura[i];
+        }
+        if(sexo[i] == "m"){
+            sm++;
+        }else{
+            sf++;
+        }
+    }
+    mediap = somap / sf;
+    mediaa = somaa / sm;
+    float maior = 0;
+    float maiora = 0;
+    string maiorp = "a";
+    string maioral = "b";
+    for(i = 0; i<4; i++){
+        
+        if(peso[i] > maior && (sexo[i] == "f")){
+            maiorp = nome[i];
+            maior = peso[i];
+        }
+        if(altura[i] > maiora && (sexo[i] == "m")){
+            maioral = nome[i];
+            maiora = peso[i];
+        }
+    }
+    string mediaps, mediaas;
+    mediaps = Convert.ToString(mediap);
+    mediaas = Convert.ToString(mediaa);
+
+    string[] saidas = new string[4]{mediaps, mediaas, maiorp, maioral};
+    Console.WriteLine($"a media da altura dos homens: {saidas[1]}\na media do peso das mulheres{saidas[0]}\no homem mais alto é {saidas[3]}\na mulher mais gorda é {saidas[2]}");
+  }
+}
 
 
