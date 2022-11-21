4de2c819cf74d3638f5662749a59c7733f7fff0d
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2022-11-20 23:14:34 -0300

    Update Lista_6_respostas.cs

--- Lista_6/Lista_6_respostas.cs
+++ Lista_6/Lista_6_respostas.cs
@@ -16,3 +16,33 @@ class q1 {
     Console.WriteLine($"O resultado das multiplicaçoes é: {multiplicaçao} ");
   }
 }
+/* Questão 2 */
+using System;
+class q2 {
+  static void Main() {
+    
+    float[] vetorA = new float[8];
+    float[] vetorB = new float[8];
+    float[] vetorC = new float[8];
+    int i = 0;
+    float soma = 0;
+    
+    for(i = 0; i < 8; i++)
+    {
+    Console.WriteLine("Digite seus 8 valores do vetor A, sendo esse o seu {0}° valor ", i + 1);
+    vetorA[i] = float.Parse(Console.ReadLine());
+    
+    Console.WriteLine("Digite seus 8 valores do vetor B, sendo esse o seu {0}° valor ", i + 1);
+    vetorB[i] = float.Parse(Console.ReadLine());
+    
+    soma = vetorA[i] + vetorB[i];
+    vetorC[i] = soma;
+   
+    }
+    
+    for(i = 0; i < 8; i++)
+    {
+    Console.WriteLine("Os valores do vetor C são: {0} ", vetorC[i]);
+    }
+  }
+}
