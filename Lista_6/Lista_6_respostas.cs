fb01b6e66e3ebcf5fc47d475d486cfdb4d29d032
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2022-11-20 23:05:07 -0300

    Update Lista_6_respostas.cs

--- Lista_6/Lista_6_respostas.cs
+++ Lista_6/Lista_6_respostas.cs
@@ -1,1 +1,18 @@
-
+/* Questão 1 */
+using System;
+class q1 {
+  static void Main() {
+    
+    float[] valor = new float[8];
+    int i = 0;
+    float multiplicaçao = 1;
+    
+    for(i = 0; i < 8; i++){
+    Console.WriteLine("Digite seus 8 valores, sendo esse o seu {0}° valor ", i + 1);
+    valor[i] = float.Parse(Console.ReadLine());
+    
+    multiplicaçao = multiplicaçao * valor[i];
+    }
+    Console.WriteLine($"O resultado das multiplicaçoes é: {multiplicaçao} ");
+  }
+}
