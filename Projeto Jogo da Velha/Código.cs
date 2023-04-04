d5ff7a01f08441f098ee48252b070cfb8f4df76e
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2023-04-04 19:27:22 -0300

    Update Código.cs

--- Projeto Jogo da Velha/Código.cs
+++ Projeto Jogo da Velha/Código.cs
@@ -26,107 +26,108 @@ using System;
 namespace Jogo_da_velha
 { 
 
- public class Jogador
- {
-   private string nome;
-   private string nomeIA;
-   private int XouO;
-   private int score;
-   private int scoreIA;
-   
-   //Construtor
-   Jogador(string nome, string nomeIA, int XouO, int score, int scoreIA)
-   {
-     this.nome   = nome;
-     this.nomeIA = nomeIA;
-     this.XouO = XouO;
-     this.score = score;
-   } 
+public class Jogador
+{
+    private string nome;
+    private string nomeIA;
+    private int XouO;
+    private int score;
+    private int scoreIA;
 
-   //Métodos GET
-   public string getNome()
-   {
-      return(nome);     
-   }
+    //Construtor
+    Jogador(string nome, string nomeIA, int XouO, int score, int scoreIA)
+    {
+        this.nome = nome;
+        this.nomeIA = nomeIA;
+        this.XouO = XouO;
+        this.score = score;
+    }
 
-   public string getNomeIA()
-   {
-      return(nomeIA);     
-   }
-   
+    //Métodos GET
+    public string getNome()
+    {
+        return (nome);
+    }
+
+    public string getNomeIA()
+    {
+        return (nomeIA);
+    }
+
     public int getXouO()
     {
-        return(XouO);     
+        return (XouO);
     }
-    
+
     public int getScore()
     {
-        return(score);     
+        return (score);
     }
-     public int getScoreIA()
+    public int getScoreIA()
     {
-        return(scoreIA);     
+        return (scoreIA);
     }
 
-   //Métodos SET
-   public void setNome(string nome)
-   {
-     this.nome = nome;    
-   }
+    //Métodos SET
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
 
-   public void setNomeIA(string nomeIA)
-   {
-     this.nomeIA = nomeIA;    
-   }
-   
+    public void setNomeIA(string nomeIA)
+    {
+        this.nomeIA = nomeIA;
+    }
+
     public void setXouO(int XouO)
     {
-        this.XouO = XouO;    
+        this.XouO = XouO;
     }
-    
+
     public void setScore(int score)
     {
-        this.score = score;    
+        this.score = score;
     }
-    
+
     public void setScoreIA(int scoreIA)
     {
-        this.scoreIA = scoreIA;    
+        this.scoreIA = scoreIA;
     }
-    
-   //Destrutor
-    
+
+    //Destrutor
+
     ~Jogador()
     {
-     
-    } 
-    
-    public static void Main(){
-        
+
+    }
+
+    public static void Main()
+    {
+
         string nome;
         string nomeIA;
         int loop = 0;
         int score = 0, scoreIA = 0;
-        int XouO =0 , XouOIA = 0;
+        int XouO = 0, XouOIA = 0;
         int linha, coluna;
-        int[,] matriz = new int[3,3];
+        int[,] matriz = new int[3, 3];
         Random numlinha = new Random();
         Random numColuna = new Random();
         int linhaIA, colunaIA;
         int jogadas = 0;
-        
+
         try
         {
-            
-            Console.Write ("Qual o seu nome? ");
+
+            Console.Write("Qual o seu nome? ");
             nome = Console.ReadLine();
 
-            Console.Write ("Qual o nome de seu oponente? ");
-            nomeIA = Console.ReadLine ();
+            Console.Write("Qual o nome de seu oponente? ");
+            nomeIA = Console.ReadLine();
 
-            Console.Write ("Você quer X ou O (Digite 1 para X ou 2 para 0)? ");
+            Console.Write("Você quer X ou O (Digite 1 para X ou 2 para 0)? ");
             XouO = int.Parse(Console.ReadLine());
-            
+
             if (XouO == 1)
             {
                 XouOIA = 2;
@@ -135,121 +136,227 @@ namespace Jogo_da_velha
             {
                 XouOIA = 1;
             }
-            Jogador jogador1 = new Jogador(nome, nomeIA, XouO, score, scoreIA); 
-       
+            Jogador jogador1 = new Jogador(nome, nomeIA, XouO, score, scoreIA);
+
             Console.WriteLine("\nO seu nome é: {0}", jogador1.getNome());
-            Console.WriteLine("O nome de seu oponente é: {0}", jogador1.getNomeIA()); 
-            Console.WriteLine("Você escolheu (X ou O): {0}\n", + jogador1.getXouO());
+            Console.WriteLine("O nome de seu oponente é: {0}", jogador1.getNomeIA());
+            Console.WriteLine("Você escolheu (X ou O): {0}\n", +jogador1.getXouO());
             Console.WriteLine("Seu Score é: {0}", +jogador1.getScore());
             Console.WriteLine("O Score de {0} é: {1}\n", jogador1.getNomeIA(), jogador1.getScoreIA());
-            
+
             do
             {
-            
+
                 do
                 {
-                
-                Console.Write("Qual linha sera sua jogada? ");
-                linha = int.Parse(Console.ReadLine());
-                
-                Console.Write("Qual coluna sera sua jogada? ");
-                coluna = int.Parse(Console.ReadLine());
-                
-                linha--;
-                coluna--;
-                
-                Console.Write("\nJogando...\n");
-                        
-                if(XouO == 1)
-                {
-                matriz [linha,coluna] = 1;
-                }
-                else if (XouO == 2 )
-                {
-                matriz [linha,coluna] = 2;
-                }
-                
-                jogadas = jogadas + 1;
-                
-                do
-                {
-                linhaIA = numlinha.Next(0,3);
-                colunaIA = numColuna.Next(0,3);
-                }
-                while((matriz[linhaIA,colunaIA] == 1) || (matriz[linhaIA,colunaIA] == 2) && (jogadas != 9));
-                
-                if(XouOIA == 1)
-                {
-                    matriz [linhaIA,colunaIA] = 1;
-                }
-                else if (XouOIA == 2 )
-                {
-                    matriz [linhaIA,colunaIA] = 2;
-                }
-                
-                jogadas = jogadas + 1;
-        
-                Console.Write("\nJogo da Velha:\n");
-                
-                for(int i = 0; i < 3; i++){ 
-                    for(int j = 0; j < 3; j++){
-                
-                if(matriz[i,j] == 1)
+
+                    Console.Write("\nQual linha sera sua jogada? ");
+                    linha = int.Parse(Console.ReadLine());
+
+                    Console.Write("Qual coluna sera sua jogada? ");
+                    coluna = int.Parse(Console.ReadLine());
+
+                    linha--;
+                    coluna--;
+
+                    Console.Write("\nJogando...\n");
+
+                    if (XouO == 1)
                     {
-                        Console.Write(" X ");
+                        matriz[linha, coluna] = 1;
                     }
-                    else if(matriz[i,j] == 2)
+                    else if (XouO == 2)
                     {
-                        Console.Write(" O ");
+                        matriz[linha, coluna] = 2;
                     }
-                    else
+
+                    jogadas = jogadas + 1;
+
+                    do
                     {
-                        Console.Write("   ");
+                        linhaIA = numlinha.Next(0, 3);
+                        colunaIA = numColuna.Next(0, 3);
                     }
+                    while ((matriz[linhaIA, colunaIA] == 1) || (matriz[linhaIA, colunaIA] == 2) && (jogadas != 9));
+
+                    if (XouOIA == 1)
+                    {
+                        matriz[linhaIA, colunaIA] = 1;
+                    }
+                    else if (XouOIA == 2)
+                    {
+                        matriz[linhaIA, colunaIA] = 2;
+                    }
+
+                    jogadas = jogadas + 1;
+
+                    Console.Write("\nJogo da Velha:\n");
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+
+                            if (matriz[i, j] == 1)
+                            {
+                                Console.Write(" X ");
+                            }
+                            else if (matriz[i, j] == 2)
+                            {
+                                Console.Write(" O ");
+                            }
+                            else
+                            {
+                                Console.Write("   ");
+                            }
+                        }
+
+                        Console.Write("\n");
+                    }
+
+                    //Horizontal
+
+                    if ((matriz[0, 0] == XouO) && (matriz[0, 1] == XouO) && (matriz[0, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+
+                    }
+                    else if ((matriz[0, 0] == XouOIA) && (matriz[0, 1] == XouOIA) && (matriz[0, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if ((matriz[1, 0] == XouO) && (matriz[1, 1] == XouO) && (matriz[1, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+
+                    }
+                    else if ((matriz[1, 0] == XouOIA) && (matriz[1, 1] == XouOIA) && (matriz[1, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if ((matriz[2, 0] == XouO) && (matriz[2, 1] == XouO) && (matriz[2, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+
+                    }
+                    else if ((matriz[2, 0] == XouOIA) && (matriz[2, 1] == XouOIA) && (matriz[2, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+
+                    //Vertical
+
+                    else if ((matriz[0, 0] == XouO) && (matriz[1, 0] == XouO) && (matriz[2, 0] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 0] == XouOIA) && (matriz[1, 0] == XouOIA) && (matriz[2, 0] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 1] == XouO) && (matriz[1, 1] == XouO) && (matriz[2, 1] == XouO))
+                    {
+
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 1] == XouOIA) && (matriz[1, 1] == XouOIA) && (matriz[1, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 2] == XouO) && (matriz[1, 2] == XouO) && (matriz[2, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 2] == XouOIA) && (matriz[1, 2] == XouOIA) && (matriz[2, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+
+                    //Diagonal
+
+                    else if ((matriz[0, 0] == XouO) && (matriz[1, 1] == XouO) && (matriz[2, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+                    }
+                    else if ((matriz[2, 0] == XouO) && (matriz[1, 1] == XouO) && (matriz[0, 2] == XouO))
+                    {
+                        score++;
+                        Console.WriteLine("       \r\n  / _ \\___________/`/\\+-/\\'\\'\\\r\n\\_\\(_)/_/ Vitória -+-    -+-+-\r\n _//o\\\\_          \\'\\/+-\\/`/`/\r\n  /   \\            \\/-+--\\/`/  \n");
+                        Console.WriteLine("\nPARABÉNS!\n");
+                        break;
+                    }
+                    else if ((matriz[0, 0] == XouOIA) && (matriz[1, 1] == XouOIA) && (matriz[2, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if ((matriz[2, 0] == XouOIA) && (matriz[1, 1] == XouOIA) && (matriz[0, 2] == XouOIA))
+                    {
+                        scoreIA++;
+                        Console.WriteLine("                  \r\n            o    o\r\n   _Derrota_ )  ( \r\n{(((((((((( ( o_o)\r\n  /\\/\\/\\/\\/\\ `--   \n");
+                        Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
+                        break;
+                    }
+                    else if (jogadas == 9)
+                    {
+                        Console.WriteLine("EMPATE!\n");
+                    }
+
                 }
-                
-                Console.Write("\n");
+                while (jogadas < 9);
+
+
+                Console.WriteLine("Para encerrar o programa digite '6', caso queira continuar digite qualquer outro numero.");
+                loop = int.Parse(Console.ReadLine());
+
             }
-        
+            while (loop != 6);
         }
-        while (jogadas < 9);
-        
-        
-            Console.WriteLine("\nSe você ganhou digite '9'.\nSe você Empatou digite '8'.\nSe você perdeu digite '7'.\nPara encerrar o programa digite '6'.");
-            loop = int.Parse(Console.ReadLine());
-            
-            if(loop == 9)
-            {
-                Console.WriteLine("\nPARABÉNS PELA VITÓRIA!\n");
-                score++;
-            }
-             else if(loop == 8)
-            {
-                Console.WriteLine("EMPATE!\n");
-                scoreIA++;
-            }
-            else if(loop == 7)
-            {
-                Console.WriteLine("QUE PENA! MA PRÓXIMA VOCÊ CONSEGUE!\n");
-                scoreIA++;
-            }
-            else if(loop == 6)
-            {
-                Console.WriteLine("PROGRAMA ENCERRADO!");
-                break;
-            }
-            
-            Console.WriteLine("Seu Score é: {0}", score);
-            Console.WriteLine("O Score de {0} é: {1}\n", jogador1.getNomeIA(), scoreIA);
-        }
-        while(loop != 6);
-        
-        }
-        
-        catch(Exception erro){ 
-        Console.WriteLine("Formato de entrada errado!\n");
-        Console.WriteLine("Erro: " + erro.Message);
-        }
+        catch (Exception erro)
+        {
+            Console.WriteLine("Formato de entrada errado!\n");
+            Console.WriteLine("Erro: " + erro.Message);
         }
     }
 }
